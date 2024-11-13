1946756de08f29bb16e7627850a5b5ef2a04f697
spartan-ytruong <y.truong@c0x12c.com>
2024-11-13 16:36:46 +0700

    add release.yaml, correct README.md, and refactor to use branch gh-pages

--- tools/create_release.sh
+++ tools/create_release.sh
@@ -12,12 +12,6 @@ echo "Next release version: $RELEASE_VERSION"
 sed -i "/^version:/c version: $RELEASE_VERSION" charts/spartan/Chart.yaml
 sed -i "/^appVersion:/c appVersion: $RELEASE_VERSION" charts/spartan/Chart.yaml
 
-mkdir -p ./charts/spartan/hosting
-
-helm package charts/spartan -d ./charts/spartan/hosting
-helm repo index ./charts/spartan/hosting --merge ./charts/spartan/hosting/index.yaml
-
-git add ./charts/spartan/hosting
 git add spartan/Chart.yaml
 
 git commit -m "chore: bump version to $RELEASE_VERSION" -a
